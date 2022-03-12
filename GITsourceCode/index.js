781c70c12ea5b33287ddcada350c987b6d9900a4
anshay <anshay12345@gmail.com>
2022-03-12 16:54:40 +0530

    placement page added

---
+++ GITsourceCode/index.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const app = express()
+
+app.get('/', (req, res)=>{
+    console.log("Index page logged in the logs")
+    res.send("<h1>WELCOME TO THE NODE JS APP</h1>")
+})
+
+app.get('/about', (req, res)=>{
+    console.log("About Page logged in the logs")
+    res.send("<h1>THIS IS THE ABOUT PAGE</h1>")
+})
+
+app.get('/home', (req, res)=>{
+    console.log("HOME Page logged in the logs")
+    res.send("<h1>THIS IS THE HOME PAGE</h1>")
+})
+
+app.listen(3000)
