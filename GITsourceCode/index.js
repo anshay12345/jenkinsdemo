9fe6027285a56afd2e1655bece3a67a83c24c280
anshay <anshay12345@gmail.com>
2022-03-12 16:59:25 +0530

    placement page added

--- GITsourceCode/index.js
+++ GITsourceCode/index.js
@@ -15,5 +15,9 @@ app.get('/home', (req, res)=>{
     console.log("HOME Page logged in the logs")
     res.send("<h1>THIS IS THE HOME PAGE</h1>")
 })
+app.get('/placement', (req, res)=>{
+    console.log("Placement Page logged in the logs")
+    res.send("<h1>THIS IS THE placement PAGE</h1>")
+})
 
 app.listen(3000)
